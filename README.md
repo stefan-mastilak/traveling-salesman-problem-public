42f05cbb5f75e806d6953861fea96ed11e541213
Stefan Mastilak <stefan.mastilak@visma.com>
2022-05-27 14:00:49 +0200

    'testing'

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# Flights assessment
+
+# Flight search assessment
+
+* Author: Stefan Mastilak
+* Contact: stefan.mastila@gmail.com
 
 ### General:
 
-* Python: 3.9
+ Solution for searching flights based on data fetched from the .csv file stored in examples folder.
+
+* Python version: 3.9
+
+
+### Usage examples:
+
+```shell
+# To filter flights stored in example3.csv from WUE to JBN with 2 pieces of baggage and maximum of 1 connection:
+
+    python -m solution example3.csv WUE JBN --bags=2 --max_connections=1
+    
+    python -m solution example3.csv WUE JBN -b=2 -m=1
+```
--- solution.py
+++ solution.py
@@ -11,19 +11,25 @@ from lib.search_engine import Searcher
 if __name__ == '__main__':
     """
     argument parser for solution.py:
+    NOTE: Usage is described in README.md file 
     """
+
+    # Create parser:
     parser = argparse.ArgumentParser()
-    # Add an arguments
-    parser.add_argument('--data', type=str, required=True, help="data source")
-    parser.add_argument('--org', type=str, required=True, help="origin airport code")
-    parser.add_argument('--des', type=str, required=True, help="destination airport code")
-    parser.add_argument('--bags', type=int, required=False, default=0, help="number of baggage pieces (default 0)")
-    parser.add_argument('--max_conns', type=int, required=False, default=2, help="maximum of connections (default 2)")
+
+    # Add arguments:
+    parser.add_argument('data', type=str, help="data source file")
+    parser.add_argument('org', type=str, help="origin airport code")
+    parser.add_argument('des', type=str, help="destination airport code")
+    parser.add_argument('-b', '--bags', type=int, required=False, default=0,
+                        help="number of baggage pieces (default 0)")
+    parser.add_argument('-m', '--max_connections', type=int, required=False, default=2,
+                        help="maximum connections (default 2)")
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Get flights:
-    results = Searcher(args.data).search(org=args.org, des=args.des, bags=args.bags, max_conns=args.max_conns)
+    results = Searcher(args.data).search(org=args.org, des=args.des, bags=args.bags, max_conns=args.max_connections)
     print(json.dumps(results, indent=4, sort_keys=False))
     print(f'\nResults count: {len(results)}')
